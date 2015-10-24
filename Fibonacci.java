1343a65903b5d19108cd847ce39580179d305ad9
JM Blessie Pineda <jmblessiepineda@yahoo.com>
2015-10-24 23:59:31 +0800

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci {
+	public static int fib(int n) {
+                if (n < 2) {
+                   return n;
+                }
+                else {
+		   return fib(n-1)+fib(n-2);
+                }
+	}
+
+        test main    
+}
